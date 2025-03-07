2b6b1b6955542f595158ae370b78f3aeffdaf5cd
ujkkju117 <ujkkju117@outlook.com>
2025-03-07 23:26:08 +1030

    Add project files.

---
+++ DiceColorSyncPlugin/DiceColorSyncPlugin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using BepInEx;
+using HarmonyLib;
+
+namespace DiceCallbackPlugin
+{
+
+    [BepInPlugin(Guid, "HolloFoxes' Dice Color Sync Plug-In", Version)]
+    public class DiceCallbackPlugin : BaseUnityPlugin
+    {
+        // constants
+        public const string Guid = "org.hollofox.plugins.DiceColorSyncPlugin";
+        private const string Version = "1.0.0.0";
+
+        /// <summary>
+        /// Awake plugin
+        /// </summary>
+        void Awake()
+        {
+            Debug.Log("DiceColorSync Plug-in loaded");
+            var harmony = new Harmony(Guid);
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ DiceColorSyncPlugin/Patches/DicePatch.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace DiceCallbackPlugin.Patches
+{
+    [HarmonyPatch(typeof(Die), "OnPhotonInstantiate")]
+    public class DicePatch
+    { 
+        static void Postfix(PhotonMessageInfo info, ref Renderer ___dieRenderer)
+        {
+            if (BoardSessionManager.PhotonIdToClientGuid.TryGetValue(info.sender.ID, out var clientId))
+                if (TempClientColorManager.TryGetColor(clientId, out var color))
+                    ___dieRenderer.material.SetColor("_Color", Color.red);
+        }
+    }
+
+    [HarmonyPatch(typeof(Die), "Init")]
+    public class DicePatch2
+    {
+        static void Postfix(ClientGuid clientId, ref Renderer ___dieRenderer)
+        {
+            if (TempClientColorManager.TryGetColor(clientId, out var color))
+                ___dieRenderer.material.SetColor("_Color", color);
+        }
+    }
+
+    [HarmonyPatch(typeof(Die), "SetMaterial")]
+    public class SetMaterialPatch
+    {
+        static bool Prefix(ref Renderer ___dieRenderer, ref bool gmDie, Material ___normalMaterial, Material ___gmMaterial)
+        {
+            if (gmDie)
+            {
+                if (!((UnityEngine.Object)___dieRenderer.sharedMaterial != (UnityEngine.Object)___gmMaterial))
+                    return false;
+
+                var color = ___dieRenderer.material.GetColor("_Color");
+                ___dieRenderer.sharedMaterial = ___gmMaterial;
+                ___dieRenderer.material.SetColor("_Color", color);
+            }
+            else
+            {
+                if (!(___dieRenderer.sharedMaterial != ___normalMaterial))
+                    return false;
+
+                var color = ___dieRenderer.material.GetColor("_Color");
+                ___dieRenderer.sharedMaterial = ___normalMaterial;
+                ___dieRenderer.material.SetColor("_Color", color);
+            }
+
+            return false;
+        }
+    }
+
+
+}
